443ec72d4677a6c92ce16e40d3a23fe713b94320
Rizal Zulkarnain <resetdariawal@gmail.com>
2021-07-11 17:19:09 +0700

    fix(api):adding validation numeric when adding data property

--- apps/zidence-api/src/app/middlewares/properties/route.ts
+++ apps/zidence-api/src/app/middlewares/properties/route.ts
@@ -9,12 +9,23 @@ router.get('/', getProperties)
 router.post('/', [
   check('name', 'Name Field is required')
     .isLength({ min: 3 }),
+  check('price', 'Price Field is required').isNumeric(),
   check('category', 'Category Field is required')
     .isLength({ min: 3 }),
-  check('subcategory', 'subcategory Field is required')
+  check('subcategory', 'Subcategory Field is required')
     .isLength({ min: 3 }),
   check('developer', 'Developer Field is required')
     .isLength({ min: 3 }),
+  check('yearBuilt', 'Year of Built Field is required')
+    .isNumeric(),
+  check('lotSize', 'lotSize Field is required')
+    .isNumeric(),
+  check('unitSize', 'unitSize Field is required')
+    .isNumeric(),
+  check('numberOfBedrooms', 'numberOfBedrooms Field is required')
+    .isNumeric(),
+  check('numberOfBathrooms', 'numberOfBathrooms Field is required')
+    .isNumeric(),
   check('owner', 'Owner Field is required')
     .isLength({ min: 3 })
 ], addProperty)

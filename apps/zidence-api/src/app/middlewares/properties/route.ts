cfc92df992fba58e61c8dcaa75a54101d10c6e0b
Rizal Zulkarnain <resetdariawal@gmail.com>
2021-07-11 16:56:14 +0700

    fix(api):refactor validation when post data property

--- apps/zidence-api/src/app/middlewares/properties/route.ts
+++ apps/zidence-api/src/app/middlewares/properties/route.ts
@@ -8,35 +8,14 @@ import { getProperties, addProperty } from './controller'
 router.get('/', getProperties)
 router.post('/', [
   check('name', 'Name Field is required')
-    .not()
-    .isEmpty()
     .isLength({ min: 3 }),
-  check('price', 'Price Field is required')
-    .not()
-    .isEmpty(),
-  check('category', 'Category Field is required').not()
-    .isEmpty()
+  check('category', 'Category Field is required')
     .isLength({ min: 3 }),
-  check('subcategory', 'subcategory Field is required').not()
-    .isEmpty()
+  check('subcategory', 'subcategory Field is required')
     .isLength({ min: 3 }),
-  check('developer', 'Developer Field is required').not()
-    .isEmpty()
+  check('developer', 'Developer Field is required')
     .isLength({ min: 3 }),
-  check('yearBuilt', 'yearBuilt Field is required').not()
-    .isEmpty(),
-  check('lotSize', 'lotSize Field is required').not()
-    .isEmpty(),
-  check('unitSize', 'unitSize Field is required').not()
-    .isEmpty(),
-  check('numberOfBedrooms', 'numberOfBedrooms Field is required').not()
-    .isEmpty(),
-  check('numberOfBathrooms', 'numberOfBathrooms Field is required').not()
-    .isEmpty(),
-  check('owner', 'Owner Field is required').not()
-    .isEmpty()
-    .isLength({ min: 3 }),
-  check('listOfNearestObjects', 'listOfNearestObjects Field is required')
+  check('owner', 'Owner Field is required')
     .isLength({ min: 3 })
 ], addProperty)
 
